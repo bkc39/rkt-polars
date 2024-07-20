4b099c0b7a7a40b73576a09b2cc37d7be0d90904
Ben Carriel <pl.proofs@gmail.com>
2024-07-20 15:37:55 -0400

    [#3] Add polars and chrono dependencies; add make_df function with DataFrame creation

--- compat/src/lib.rs
+++ compat/src/lib.rs
@@ -1,3 +1,6 @@
+use chrono::prelude::*;
+use polars::prelude::*;
+
 #[no_mangle]
 pub extern "C" fn add(left: usize, right: usize) -> usize {
     left + right
@@ -12,4 +15,19 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+}
+
+#[no_mangle]
+pub extern "C" fn make_df() {
+    df!(
+        "integer" => &[1, 2, 3],
+        "date" => &[
+                NaiveDate::from_ymd_opt(2025, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2025, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2025, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+        ],
+        "float" => &[4.0, 5.0, 6.0],
+        "string" => &["a", "b", "c"],
+    )
+    .unwrap();
 }
