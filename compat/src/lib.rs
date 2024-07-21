303469c91df1a6d3ec519b0b4b5668b50ead5bd6
Ben Carriel <pl.proofs@gmail.com>
2024-07-21 17:49:03 -0400

    [#3] 80 char

--- compat/src/lib.rs
+++ compat/src/lib.rs
@@ -23,9 +23,12 @@ pub extern "C" fn make_df() -> *mut DataFrame {
     let df = df!(
         "integer" => &[1, 2, 3],
         "date" => &[
-                NaiveDate::from_ymd_opt(2025, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                NaiveDate::from_ymd_opt(2025, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                NaiveDate::from_ymd_opt(2025, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2025, 1, 1)
+                .unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2025, 1, 2)
+                .unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2025, 1, 3)
+                .unwrap().and_hms_opt(0, 0, 0).unwrap(),
         ],
         "float" => &[4.0, 5.0, 6.0],
         "string" => &["a", "b", "c"],
