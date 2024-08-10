bd6629e343d1503b5f80f39f8c5b599425280530
Ben Carriel <pl.proofs@gmail.com>
2024-08-10 11:07:22 -0400

    [#4] Update FFI bindings and installer checks
    - Replaced direct string path with `define-runtime-path` for `libcompat`.
    - Temporarily disable `add` function and related tests.
    - Added `hello_world` function.
    - Adjusted pre-installer script to handle errors and copy shared objects.

--- polars/main.rkt
+++ polars/main.rkt
@@ -1,20 +1,31 @@
 #lang racket/base
 
 (require ffi/unsafe
-         ffi/unsafe/define)
+         ffi/unsafe/define
+         racket/runtime-path
+         (for-syntax racket/base))
+
+(provide (all-defined-out))
 
 (module+ test
   (require rackunit))
 
+(define-runtime-path libcompat
+  '(so "libcompat"))
+
 (define-ffi-definer define-compat
-  (ffi-lib "libcompat.so"))
+  (ffi-lib libcompat))
 
-(define-compat add
-  (_fun _uint32 _uint32 -> _uint32))
+;; (define-compat add
+;;   (_fun _uint32 _uint32 -> _uint32))
 
-(module+ test
-  (check-equal? (add 770 7) 777))
+(define-compat hello-world
+  (_fun -> _void)
+  #:c-id hello_world)
 
-(module+ main
-  (printf "~a + ~a = ~a~n"
-          1700 29 (add 1700 29)))
+;; (module+ test
+;;   (check-equal? (add 770 7) 777))
+
+;; (module+ main
+;;   (printf "~a + ~a = ~a~n"
+;;           1700 29 (add 1700 29)))
--- polars/private/install-compat.rkt
+++ polars/private/install-compat.rkt
@@ -1,19 +1,22 @@
 #lang racket/base
 
-(require racket/file
-         racket/system)
+(require
+ (only-in dynext/file
+          append-extension-suffix)
+ (only-in racket/system
+          system*)
+ (only-in setup/dirs
+          find-user-lib-dir
+          find-lib-dir))
 
 (provide pre-installer)
 
 (define COMPAT-SRC-DIR "compat")
 
+(define (preinstall-error . args)
+  (apply error (cons 'pre-installer args)))
+
 (define (pre-installer collections-top-path this-collection-path user-specific?)
-  (printf "top: ~a~nthis: ~a~nspecific? ~a~n"
-          collections-top-path
-          this-collection-path
-          user-specific?)
-
-
   (define private-path
     (build-path this-collection-path "private"))
   (define compat-path
@@ -21,13 +24,33 @@
   (define cargo-path
     (find-executable-path "cargo"))
   (unless (path? cargo-path)
-    (error
-     'pre-installer
+    (preinstall-error
      "cargo command not found on system. install cargo:~n~a"
      "https://doc.rust-lang.org/cargo/getting-started/installation.html"))
+  (define shared-object-basename
+    (append-extension-suffix "libcompat"))
+  (define compiled-object-path
+    (build-path compat-path
+                "target"
+                "release"
+                shared-object-basename))
+  (define lib-path
+    (if user-specific?
+        (find-user-lib-dir)
+        (find-lib-dir)))
+  (define destination-object-path
+    (build-path lib-path shared-object-basename))
 
   (parameterize ([current-directory compat-path])
     (displayln (format "in directory: ~a" (current-directory)))
     (unless (system* cargo-path "build" "--release")
-      (error 'pre-installer "cargo build failed"))
-    (displayln "here we are")))
+      (preinstall-error "cargo build failed"))
+    (unless (file-exists? compiled-object-path)
+      (preinstall-error
+       "libcompat shared object not in expected location:~n~a"
+       compiled-object-path))
+    (copy-file compiled-object-path destination-object-path
+               #:exists-ok? #t)
+    (printf "Copied ~a => ~a\n"
+            compiled-object-path
+            destination-object-path)))
